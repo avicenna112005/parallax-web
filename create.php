fcc1832642d3e898a7532b1e04725107cf8f8b54
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2022-09-30 08:04:41 +0700

    tambah create

--- create.php
+++ create.php
@@ -186,7 +186,7 @@
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Data Produk</h1>
+                    <h1 class="h3 mb-4 text-gray-800">Tambah Data Produk</h1>
 
                     <!-- isi disini -->
                 </div>
