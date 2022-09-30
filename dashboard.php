2439a319076a84f6c9ff6f9e1c22dd7ae0891cf4
avicenna112005 <avicennaputra112005@gmail.com>
2022-09-30 09:37:54 +0700

    edit

--- dashboard.php
+++ dashboard.php
@@ -62,16 +62,16 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
-                    <span>masih ndak tau isi apa</span>
+                    <span>Action</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
+                        <a class="collapse-item" href="utilities-color.html">Edit Data Produk</a>
+                        <a class="collapse-item" href="utilities-border.html">Edit Data Barang </a>
+                        <a class="collapse-item" href="utilities-animation.html">Tambah Data Produk</a>
+                        <a class="collapse-item" href="utilities-other.html">Tambah Data Barang</a>
                     </div>
                 </div>
             </li>
