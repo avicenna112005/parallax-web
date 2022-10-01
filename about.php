cb6f30543f92e56e03a88c0be3669b8a6f593336
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2022-10-01 14:29:52 +0700

    tsaqif

--- about.php
+++ about.php
@@ -4,13 +4,64 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DistincTrends - About</title>
+    <link rel="stylesheet" href="about.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?  family=Quicksand&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>About Us</h1>
+    <div class="container">
+    
+        <!-- navbar -->
+        <div class= navbar>
+            <p>DistinctTrends</p>
+
+            <div class="nav-link">
+                <div><a href="http://">Home</a></div>
+                <div><a href="http://">Brand</a></div>
+                <div><a href="http://">About</a></div>
+                <div><a href="http://">Contact</a></div>
+            </div>
+        </div>
+
+    </div>
     <div class="wrap">
+        <h1 style="font-size: 40px;">About Us</h1>
         <p>DistinctTrends pertama kali didirikan pada tahun 2005. Selama bertahun-tahun, DistinctTrends telah mengalami perubahan yang luar biasa untuk menjadi lebih etis dan relevan di era baru industri mode seni lokal dan internasional. Dengan dua lokasi fisik: satu di JL Sudirman Jakarta dan di kota Bandung. Staf yang ramah, dikombinasikan dengan lingkungan yang nyaman dan ramah.</p>
         <p>Perusahaan fashion dengan warisan yang kuat dan tujuan yang lebih tinggi - untuk memberdayakan dan menginspirasi wanita dan pria di mana pun. Itulah kita</p>
     </div>
+    <img src="mdl.jpg" alt="" id="model">
+    <footer class="footer">
+        <p style="color: #fff;">DistinctTrends</p>
+        <div class="foot-link">
+            <ul>
+                <li>
+                    <a href="">English</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+                <li>
+                    <a href="">Stores</a>
+                </li>
+                <li>
+                    <a href="">Apps</a>
+                </li>
+                <li>
+                    <a href="">Follow Us</a>
+                </li>
+                <li>
+                    <a href="">Legal & Privacy</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Sitemap</a href="">
+                </li>
+            </ul>
+        </div>
+</footer>
 </body>
 </html>
