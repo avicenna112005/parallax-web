bf73c18daed336a4d7a3f53c7a118b7f2d3e27ba
fauzankirana160526 <kiranafauzan6@gmail.com>
2022-10-01 15:13:26 +0700

    update About fauzan

--- about.php
+++ about.php
@@ -5,18 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DistincTrends - About</title>
-    <link rel="stylesheet" href="about.css">
+    <style>
+        <?php include 'about.css'
+    ?>
+    </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@300&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?  family=Quicksand&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="container">
-    
         <!-- navbar -->
         <div class= navbar>
             <p>DistinctTrends</p>
-
             <div class="nav-link">
                 <div><a href="http://">Home</a></div>
                 <div><a href="http://">Brand</a></div>
@@ -26,33 +30,16 @@
         </div>
 
     </div>
-<<<<<<< HEAD
-    <div class="ft">
-        <p>DistinctTrends</p>
-        <div class="grs2">
-        </div>
-        <div class="txt135">
-            <div class="txt145">
-                <p>Englis</p>
-                <p>Contact</p>
-                <p>Stores</p>
-                <p>Apps</p>
-                <p>Follow Us</p>
-                <p>Legal & Privacy</p>
-                <p>About</p>
-                <p>Sitemap</p>
-            </div>  
-        </div>
-=======
     <div class="wrap">
         <h1 style="font-size: 40px;">About Us</h1>
         <p>DistinctTrends pertama kali didirikan pada tahun 2005. Selama bertahun-tahun, DistinctTrends telah mengalami perubahan yang luar biasa untuk menjadi lebih etis dan relevan di era baru industri mode seni lokal dan internasional. Dengan dua lokasi fisik: satu di JL Sudirman Jakarta dan di kota Bandung. Staf yang ramah, dikombinasikan dengan lingkungan yang nyaman dan ramah.</p>
         <p>Perusahaan fashion dengan warisan yang kuat dan tujuan yang lebih tinggi - untuk memberdayakan dan menginspirasi wanita dan pria di mana pun. Itulah kita</p>
->>>>>>> cb6f30543f92e56e03a88c0be3669b8a6f593336
+
     </div>
     <img src="mdl.jpg" alt="" id="model">
     <footer class="footer">
         <p style="color: #fff;">DistinctTrends</p>
+        <div class="border1"></div>
         <div class="foot-link">
             <ul>
                 <li>
