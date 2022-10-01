1ea129dbd4169bb6be15334e8a39b13b04f8749f
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2022-10-01 13:15:28 +0700

    tsaqif

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>About Us</h1>
+    <div class="wrap">
+        <p>DistinctTrends pertama kali didirikan pada tahun 2005. Selama bertahun-tahun, DistinctTrends telah mengalami perubahan yang luar biasa untuk menjadi lebih etis dan relevan di era baru industri mode seni lokal dan internasional. Dengan dua lokasi fisik: satu di JL Sudirman Jakarta dan di kota Bandung. Staf yang ramah, dikombinasikan dengan lingkungan yang nyaman dan ramah.</p>
+        <p>Perusahaan fashion dengan warisan yang kuat dan tujuan yang lebih tinggi - untuk memberdayakan dan menginspirasi wanita dan pria di mana pun. Itulah kita</p>
+    </div>
+</body>
+</html>
